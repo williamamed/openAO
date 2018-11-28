b86a75722aaff3bff6407d0c0fa0f5b56f71530f
William Amed <william.tamayo@ltu.jovenclub.cu>
2018-11-28 09:20:34 -0500

    fix compress

--- src/Raptor2/SyntarsusBundle/Compiler/Compiler.php
+++ src/Raptor2/SyntarsusBundle/Compiler/Compiler.php
@@ -10,32 +10,32 @@ class Compiler extends Resources{
     
     public function create() {
         $this->setAsset('Category/js/all.js')
-                ->add('Category/js/all-classes.js')
+                ->add('Raptor2/Syntarsus/Category/js/all-classes.js',false)
                 ->add('Category/js/app.js')
                 ->compile(Resources::JS);
         
         $this->setAsset('Auditories/js/all.js')
-                ->add('Auditories/js/all-classes.js')
+                ->add('Raptor2/Syntarsus/Auditories/js/all-classes.js',false)
                 ->add('Auditories/js/app.js')
                 ->compile(Resources::JS);
 		
 	$this->setAsset('estructure/js/all.js')
-                ->add('estructure/js/all-classes.js')
+                ->add('Raptor2/Syntarsus/estructure/js/all-classes.js',false)
                 ->add('estructure/js/app.js')
                 ->compile(Resources::JS);
         
         $this->setAsset('privilege/js/all.js')
-                ->add('privilege/js/all-classes.js')
+                ->add('Raptor2/Syntarsus/privilege/js/all-classes.js',false)
                 ->add('privilege/js/app.js')
                 ->compile(Resources::JS);
         
         $this->setAsset('rol/js/all.js')
-                ->add('rol/js/all-classes.js')
+                ->add('Raptor2/Syntarsus/rol/js/all-classes.js',false)
                 ->add('rol/js/app.js')
                 ->compile(Resources::JS);
         
         $this->setAsset('user/js/all.js')
-                ->add('user/js/all-classes.js')
+                ->add('Raptor2/Syntarsus/user/js/all-classes.js',false)
                 ->add('user/js/app.js')
                 ->compile(Resources::JS);
         
